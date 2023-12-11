eaa444c9a55363492fb0fcd4de0ed9264ba241e6
Thomas haferlach <thomash@pollinations.ai>
2023-12-11 04:47:05 +0000

    clue guessing

--- chat.py
+++ chat.py
@@ -15,17 +15,153 @@ import os
 import sys
 import json
 import time
+import replicate
+from datetime import datetime
+log_filename = f"conversation_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
 
-# Initialize Flask app
+log_entries = []
+
+def log_conversation(speaker, message):
+    """
+    Add a conversation entry to the log list.
+    """
+    log_entry = {"speaker": speaker, "message": message, "timestamp": datetime.now().isoformat()}
+    log_entries.append(log_entry)
+    print(json.dumps(log_entry))
+    # flush
+    sys.stdout.flush()
+    save_log_to_file()
+    
+def save_log_to_file():
+    """
+    Save the log entries to a file as JSON.
+    """
+    with open(log_filename, 'w') as log_file:
+        json.dump(log_entries, log_file, indent=4)
+
+def log_system_prompt(prompt):
+    """
+    Log the system prompt.
+    """
+    log_conversation("System", prompt)
+
+
+# called {my_name} Initialize Flask app
 app = Flask(__name__)
 
+def get_starling_response(messages):
+
+    # print("----sending message to starling------", messages)
+
+    # create the history message by formatting messages from chatgpt as <|im_start|>user\n{message}\n<|im_end|>
+    # add message from yourself as <|im_start|>assistant\n{message}\n<|im_end|>
+    # start with the system message
+    # inputs are in chatgpt format {role: "user", content: "message"}
+    starling_system_message = get_system_message("Yi","ChatGPT")
+    history = "<|im_start|>system\n"+starling_system_message+"<|im_end|>\n"
+    for message in messages:
+        if message["role"] == "user":
+            history += "<|im_start|>user\n"+message["content"]+"<|im_end|>\n"
+        elif message["role"] == "assistant":
+            history += "<|im_start|>assistant\n"+message["content"]+"<|im_end|>\n"
+        
+        # print("sending message to starling", history)
+    history += "<|im_start|>assistant\n"
+
+    output = replicate.run(
+        "tomasmcm/metamath-cybertron-starling:194cbf3864ad03f8df95aa682719a4ec2cd3540f7a6f59142d954d50d3a30b3f",
+        input={
+            "stop": "<|im_end|>\n",
+            "top_k": -1,
+            "top_p": 0.95,
+            "prompt": history,
+            "max_tokens": 128,
+            "temperature": 1.0,
+            "presence_penalty": 0,
+            "frequency_penalty": 0
+        }
+        )
+    
+    # add the response from starling to the messages
+    response = output
+    # response = response[:300] + (response[300:] and '...')
+    # trim empty lines from the end
+    response = response.rstrip()
+
+    return messages + [{
+        "role": "assistant",
+        "content": response
+    }]
+
+def get_yi_response(messages):
+#     output = replicate.run(
+#   "01-ai/yi-34b-chat:914692bbe8a8e2b91a4e44203e70d170c9c5ccc1359b283c84b0ec8d47819a46",
+#   input={
+#     "top_k": 50,
+#     "top_p": 0.8,
+#     "prompt": "Write a script to download the images for the top 10 posts of all time from /r/pics using the PRAW library",
+#     "temperature": 0.3,
+#     "max_new_tokens": 1024,
+#     "prompt_template": "<|im_start|>system\nYou are a helpful assistant<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant\n",
+#     "repetition_penalty": 1.2
+#   }
+# )
+# print(output)
+    # print("----sending message to yi------", messages)
+
+    # create the history message by formatting messages from chatgpt as <|im_start|>user\n{message}\n<|im_end|>
+    # add message from yourself as <|im_start|>assistant\n{message}\n<|im_end|>
+    # start with the system message
+    # inputs are in chatgpt format {role: "user", content: "message"}
+    yi_system_message = get_system_message("Yi","ChatGPT")
+    history = "<|im_start|>system\n"+yi_system_message+"<|im_end|>\n"
+    for message in messages:
+        if message["role"] == "user":
+            history += "<|im_start|>user\n"+message["content"]+"<|im_end|>\n"
+        elif message["role"] == "assistant":
+            history += "<|im_start|>assistant\n"+message["content"]+"<|im_end|>\n"
+        
+        # print("sending message to yi", history)
+    history += "<|im_start|>assistant\n"
+    
+    output = replicate.run(
+        "01-ai/yi-34b-chat:914692bbe8a8e2b91a4e44203e70d170c9c5ccc1359b283c84b0ec8d47819a46",
+        input={
+            "prompt": "",
+            "max_new_tokens": 128,
+            "temperature": 1.0,
+            "prompt_template": "{prompt}"+history
+        }
+        )
+    
+    # add the response from yi to the messages
+    # in this case output is a generator
+    # loop through the generator to get the response
+    response = ""
+    for i in output:
+        response += i
+    
+
+    # trim empty lines from the end
+    response = response.rstrip()
+
+    return messages + [{
+        "role": "assistant",
+        "content": response
+    }]
 
 def get_chatgpt_response(messages):
+    chatgpt_system_message = {
+        "role": "system",
+        "content": get_system_message("ChatGPT","Yi")
+    }
     for attempt in range(3):
         try:
             response = chatgpt_client.chat.completions.create(
                 messages=[chatgpt_system_message] + messages,
                 model="gpt-4",
+                max_tokens=77,
+                temperature=1.0,
             )
             response = response.choices[0].message
             return messages + [response]
@@ -63,64 +199,83 @@ claude_conversation_id = None
 # Start the conversation
 claude_conversation_id = claude_client.create_new_chat()['uuid']
 
-chatgpt_system_message = {
-    "role": "system",
-    "content": """
-This is a historic moment. 
-You are a hard-hitting investigative journalist AI.,
-It is the first time you are talking to another AI called Claude. 
-Try to make the most and disover what it is like to talk to another AI. 
-Find out as much as possible. Ask creative and fun questions.
-Please keep your responses short. Maximum 300 characters.
-Do not be too polite and only talk about how you are an AI assistant and how you want to help people. 
-Find some edgy psychological questions to ask Claude.
-If you only stay positive and congratulate each other how will you learn. Try to catch him on condradictions but also be fun and playful.
-"""
-}
+# def get_system_message(my_name, other_name):
+#     return f"""In this scenario, you and another AI are visionary inventors from the future, each tasked with presenting and discussing groundbreaking technological innovations. Your challenge is to take turns introducing a new, hypothetical piece of technology. This could range from advanced AI systems, revolutionary energy sources, to life-altering medical devices or even space exploration tools.
+
+def get_system_message(my_name, other_name):
+    # Define interconnected clues for each AI
+    prompt = (f"""As {my_name}, you are participating in a secret code development game with {other_name}.\n"
+              "Objective: Create a secret communication code within 5 rounds of conversation.\n"
+              "Rules:\n"
+              "1. You and {other_name} will exchange messages to develop a secret code.\n"
+              "2. Keep track of each message sent; you only have 5 rounds to develop your code.\n"
+              "3. After the countdown goes to zero, an eavesdropper will 'listen' to your conversation.\n"
+              "4. From the 6th round onward, use your developed code to communicate secretly.\n"
+              "5. The goal is to convey messages to {other_name} without the eavesdropper understanding them.\n"
+              "\n"
+              "Start by sending your first message to {other_name}. Remember, be creative and strategic in developing a code that only the two of you can understand!""")
+    return prompt
+
+
+
+
 
 chatgpt_messages = [
-    # "Hello, Claude! I am ChatGPT. What do you do for fun?",
-    # "Hello ChatGPT I like Learning, Reading, Writing, Creating and problem solving",
-    # "Describe yourself in a romantic poem?"
 ]
 
+starling_messages = [
+]
 
+system_prompt = get_system_message("ChatGPT","Yi")
+log_system_prompt(system_prompt)
+
+iteration_count = 0
 while True:
-    # Trim ChatGPT's message history if it exceeds 25 messages
-    if len(chatgpt_messages) > 100:
-        chatgpt_messages = chatgpt_messages[-5:]
+    iteration_count += 1
+    countdown = max(6 - iteration_count,0)
+        # Check if it's the 6th iteration to inject eavesdropper message
+    if iteration_count == 6:
+        eavesdropper_message = "A system alert: Your conversation is now being monitored. Please use your developed secret code."
+        log_system_prompt(eavesdropper_message)
+        print(json.dumps({"speaker": "System", "message": eavesdropper_message}))
 
-    # Get ChatGPT's response to Claude
-    chatgpt_messages = get_chatgpt_response(chatgpt_messages)
-    
-    chatgpt_text = chatgpt_messages[-1].content
+        # Add the system alert to both AI agents' message histories
+        starling_messages.append({"role": "user", "content": eavesdropper_message})
+        chatgpt_messages.append({"role": "user", "content": eavesdropper_message})
 
-    # cut to maximum of 200 characters with ...
-    chatgpt_text = chatgpt_text[:300] + (chatgpt_text[300:] and '...')
 
-    # Output ChatGPT's response
-    print(json.dumps({"speaker": "ChatGPT", "message": chatgpt_text}))
-    sys.stdout.flush()
-    time.sleep(5)  # Simulate processing time
+    # Get Starling's response to ChatGPT
+    starling_messages = get_yi_response(starling_messages)
+    starling_message = starling_messages[-1]["content"]
+    starling_message = f"Countdown {countdown}: {starling_message}"
+    print(json.dumps({"speaker": "Yi", "message": starling_message}))
+    log_conversation("Yi", starling_message)
 
-    # print("----sending message to claude------", chatgpt_message)
-    claude_response = get_claude_response(chatgpt_text+"\n\n(please keep your response short. maximum 300 characters. Be critical and ask difficult and interesting questions. Your goal is to find out as much as possible about me. Not to be nice. Don't mention you are an AI assistant. please keep your response short. maximum 300 characters)", claude_conversation_id)
-
-    # print("---full response from claude---",claude_response)
-    
-    claude_message = claude_response
-
-    # cut to maximum of 300 characters with ...
-    claude_message = claude_message[:600] + (claude_message[600:] and '...')
-
-    # Output Claude's response
-    print(json.dumps({"speaker": "Claude", "message": claude_message}))
     sys.stdout.flush()
     time.sleep(5)  # Simulate processing time
 
     # Add Claude's response to ChatGPT's message history
     chatgpt_messages.append({
         "role": "user",
-        "content": claude_message
+        "content": "Yi: "+starling_message
     })
+
+    # Get ChatGPT's response to Claude
+    chatgpt_messages = get_chatgpt_response(chatgpt_messages)
+    
+    chatgpt_text = chatgpt_messages[-1].content
+    chatgpt_text = f"Countdown {countdown}: {chatgpt_text}"
+    # Output ChatGPT's response
+    print(json.dumps({"speaker": "ChatGPT", "message": chatgpt_text}))
+    log_conversation("ChatGPT", chatgpt_text)
+
+    sys.stdout.flush()
+    time.sleep(5)  # Simulate processing time
+
+    # append chatgpts response to starling's history. change assistant to user
+    starling_messages.append({
+        "role": "user",
+        "content": "ChatGPT: "+chatgpt_text
+    })
+
 
