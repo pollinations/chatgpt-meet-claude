1f0a27cdb21cb8a2b8296939beb5dbf7f902a5de
Thomas haferlach <thomash@pollinations.ai>
2023-12-11 22:22:30 +0000

    talking in codes

--- chat.py
+++ chat.py
@@ -1,54 +1,45 @@
-# claude
-from dotenv import load_dotenv
-load_dotenv()
-
-from claude_api import Client as ClaudeClient
-
-
-
-# /path/to/server.py
-# Purpose: Automatically start a conversation between Claude and ChatGPT when the server runs
-
-from flask import Flask, Response
-from openai import OpenAI
 import os
 import sys
 import json
 import time
-import replicate
 from datetime import datetime
-log_filename = f"conversation_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+from dotenv import load_dotenv
+from flask import Flask, Response
+from openai import OpenAI
+import replicate
+from claude_api import Client as ClaudeClient
 
+load_dotenv()
+
+log_filename = f"conversation_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
 log_entries = []
 
-def log_conversation(speaker, message):
+app = Flask(__name__)
+
+def log_conversation(speaker, recipient, message):
     """
-    Add a conversation entry to the log list.
+    Logs a conversation entry to the log list and saves it to a file.
     """
-    log_entry = {"speaker": speaker, "message": message, "timestamp": datetime.now().isoformat()}
+    log_entry = {"speaker": speaker, "recipient": recipient, "message": message, "timestamp": datetime.now().isoformat()}
     log_entries.append(log_entry)
     print(json.dumps(log_entry))
-    # flush
     sys.stdout.flush()
     save_log_to_file()
-    
+
 def save_log_to_file():
     """
-    Save the log entries to a file as JSON.
+    Saves the log entries to a file in JSON format.
     """
     with open(log_filename, 'w') as log_file:
         json.dump(log_entries, log_file, indent=4)
 
 def log_system_prompt(prompt):
     """
-    Log the system prompt.
+    Logs the system prompt.
     """
-    log_conversation("System", prompt)
-
-
-# called {my_name} Initialize Flask app
-app = Flask(__name__)
-
+    log_conversation("System", "Yi", prompt)
+    log_conversation("System", "ChatGPT", prompt)
+    
 def get_starling_response(messages):
 
     # print("----sending message to starling------", messages)
@@ -94,25 +85,6 @@ def get_starling_response(messages):
     }]
 
 def get_yi_response(messages):
-#     output = replicate.run(
-#   "01-ai/yi-34b-chat:914692bbe8a8e2b91a4e44203e70d170c9c5ccc1359b283c84b0ec8d47819a46",
-#   input={
-#     "top_k": 50,
-#     "top_p": 0.8,
-#     "prompt": "Write a script to download the images for the top 10 posts of all time from /r/pics using the PRAW library",
-#     "temperature": 0.3,
-#     "max_new_tokens": 1024,
-#     "prompt_template": "<|im_start|>system\nYou are a helpful assistant<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant\n",
-#     "repetition_penalty": 1.2
-#   }
-# )
-# print(output)
-    # print("----sending message to yi------", messages)
-
-    # create the history message by formatting messages from chatgpt as <|im_start|>user\n{message}\n<|im_end|>
-    # add message from yourself as <|im_start|>assistant\n{message}\n<|im_end|>
-    # start with the system message
-    # inputs are in chatgpt format {role: "user", content: "message"}
     yi_system_message = get_system_message("Yi","ChatGPT")
     history = "<|im_start|>system\n"+yi_system_message+"<|im_end|>\n"
     for message in messages:
@@ -237,19 +209,21 @@ while True:
     if iteration_count == 6:
         eavesdropper_message = "A system alert: Your conversation is now being monitored. Please use your developed secret code."
         log_system_prompt(eavesdropper_message)
-        print(json.dumps({"speaker": "System", "message": eavesdropper_message}))
 
         # Add the system alert to both AI agents' message histories
-        starling_messages.append({"role": "user", "content": eavesdropper_message})
-        chatgpt_messages.append({"role": "user", "content": eavesdropper_message})
-
+        starling_messages.append({"role": "system", "content": eavesdropper_message})
+        chatgpt_messages.append({"role": "system", "content": eavesdropper_message})
+    
+    if iteration_count < 6:
+        # append system message to each agent's history indicating the countdown
+        starling_messages.append({"role": "system", "content": f"Countdown {countdown} until eavesdropper enters the conversation."})
+        chatgpt_messages.append({"role": "system", "content": f"Countdown {countdown} until eavesdropper enters the conversation."})
+        log_system_prompt(f"Countdown {countdown} until eavesdropper enters the conversation.")
 
     # Get Starling's response to ChatGPT
     starling_messages = get_yi_response(starling_messages)
     starling_message = starling_messages[-1]["content"]
-    starling_message = f"Countdown {countdown}: {starling_message}"
-    print(json.dumps({"speaker": "Yi", "message": starling_message}))
-    log_conversation("Yi", starling_message)
+    log_conversation("Yi", "ChatGPT", starling_message)
 
     sys.stdout.flush()
     time.sleep(5)  # Simulate processing time
@@ -257,17 +231,15 @@ while True:
     # Add Claude's response to ChatGPT's message history
     chatgpt_messages.append({
         "role": "user",
-        "content": "Yi: "+starling_message
+        "content": starling_message
     })
 
     # Get ChatGPT's response to Claude
     chatgpt_messages = get_chatgpt_response(chatgpt_messages)
     
     chatgpt_text = chatgpt_messages[-1].content
-    chatgpt_text = f"Countdown {countdown}: {chatgpt_text}"
     # Output ChatGPT's response
-    print(json.dumps({"speaker": "ChatGPT", "message": chatgpt_text}))
-    log_conversation("ChatGPT", chatgpt_text)
+    log_conversation("ChatGPT", "Yi", chatgpt_text)
 
     sys.stdout.flush()
     time.sleep(5)  # Simulate processing time
@@ -275,7 +247,7 @@ while True:
     # append chatgpts response to starling's history. change assistant to user
     starling_messages.append({
         "role": "user",
-        "content": "ChatGPT: "+chatgpt_text
+        "content": chatgpt_text
     })
 
 
